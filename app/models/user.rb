41c8c7c691a533cd59cde5664212d9a8b3f4f096
wonjongin <wji0227@naver.com>
2025-01-06 11:33:42 +0900

    [FEAT] Add total_vacations_in

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,26 @@ class User < ApplicationRecord
     }.stringify_keys!
     d[status]
   end
+
+  # 해당하는 해의 발생연차를 계산
+  # @param year [Integer] 연차를 계산할 해
+  # @return [Integer] 발생연차 수
+  def total_vacations_in(year)
+    experience = year - hire_date.year
+
+    if experience >= 2
+      [15 + (experience - 1).div(2), 25].min
+    elsif experience == 1
+      temp1 = hire_date.next_year
+      temp2 = hire_date.next_year.end_of_year
+
+      hire_date.month - 1 + (((temp2 - temp1) / 365) * 15).round
+    elsif experience == 0
+      12 - hire_date.month
+    end
+  end
+
+  def total_vacations_in_this_year
+    total_vacations_in(Date.today.year)
+  end
 end
